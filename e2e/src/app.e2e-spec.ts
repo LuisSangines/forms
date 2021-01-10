5e0d4e8e3774d46f1de45ff8cf0e37d6dfc4841c
jorge3903 <jevl290397@gmail.com>
2021-01-09 18:24:02 -0600

    first tests for UAMI students

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,20 +1,86 @@
-import { AppPage } from './app.po';
+import { ReactivePage } from './app.po';
 import { browser, logging } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+const origFn = browser.driver.controlFlow().execute;
+// esta función pone un retrazo de tiempo entre cada paso del controlFlow de protractor
+browser.driver.controlFlow().execute = function stop() {
+  const args = arguments;
 
-  beforeEach(() => {
-    page = new AppPage();
+  origFn.call(browser.driver.controlFlow(), () => {
+    return protractor.promise.delayed(0); // tiempo de retraso entre cada paso
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('formularios app is running!');
+  return origFn.apply(browser.driver.controlFlow(), args);
+};
+// puede correr la prueba con el comando ng e2e en la carpeta del proyecto, si necesita cambiar el puerto escriba ademas --port=PORTNUMBER
+describe('Casos de ejemplo', () => { // Engloba todas las pruebas (it) de un caso de uso a probar
+  let page: ReactivePage;
+
+  beforeEach(() => { // Este método se ejecuta antes de cada prueba
+    page = new ReactivePage(); // crea un objeto de la página reactive forms
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
+  // si ponemos xit o xdescribe jasmine ignorara la prueba o el conjunto de pruebas
+  it('Debe poder ir a la pagina de reactive forms', () => { // primera prueba de ejemplo
+    page.navigateToReactivePage();
+    expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + 'reactive'); // comprobamos que carge la página
+  });
+
+  it('Debe mostrar el título de la página', async () => {
+    page.navigateToReactivePage();
+    const title = await page.getTitleText();
+    await console.log(title); // podemos imprimir el texto en consola para debuggin
+    expect(title).toEqual('Formularios Reactivos'); // comprobamos que el titulo renderizado sea el que esperamos
+  });
+
+  it('Debe escribir Jorge en el campo nombre', async () => { // en este ejemplo podemos quitar el método waitForAngular y los await
+    page.navigateToReactivePage();                           // pero los dejo por que me parece mas ilustrativo para futuras prácticas
+    await browser.waitForAngular(); // espera que angular este estable (termine de cargar la página)
+    await page.setName('Jorge');
+    expect(page.getName()).toEqual('Jorge'); // comprobamos que el input tenga el valor que le acabamos de escribir
+  });
+
+  it('Debe mostrar un error en el campo apellido si presionamos guardar', () => {
+    page.navigateToReactivePage();
+    page.clickSaveButton();
+    expect(page.getTextOfEspecificError(0)).toEqual('Apellido obligatorio :O'); // esperamos que aparezca el error
+  });
+
+  it('Debe mostrar un error si el campo nombre tiene menos 4 letras', () => {
+    page.navigateToReactivePage();
+    page.setName('Jorg');
+    expect(page.getTextOfEspecificError(0)).toEqual('Ingresa al menos 5 letras :)'); // esperamos que aparezca el error
+  });
+
+  it('Debe agregar un elemento a la tabla hobbies si presionamos Agregar', async () => {
+    page.navigateToReactivePage();
+    await page.clickAddButton();
+    expect(page.deleteHobbieButtonIsPresent()).toBeTruthy(); // esperamos que el botón este presente en la página
+  });
+
+  // ******* EJERCICIOS PARA PRACTICAR ************
+  // 1. llenar todos los campos del formulario reactivo correctamente, presionar el botón guardar y verificar que se
+  // limpian todos los campos del formulario
+  // 2. Probar que el botón de borrar hobbie funciona
+  // 3. Crear el arreglo ['jorge', 'jorge@' , 'jorge@ho'] hacer una prueba que escriba cada uno de los
+  // string del arreglo en el campo correo y verificar que el mensaje de error aparezca en la pantalla
+  // (Se debe utilizar un for para no repetir el código por cada string en el arreglo)
+  // 4. Crear 2 archivos uno para la clase formulario template y otro para el describe de las pruebas de esa página
+  // (el archivo debe terminar con extención .e2e-spec.ts), recuerden hacer un método en la clase formulario template
+  // que se dirija a dicha página del proyecto con la instrucción browser.get(browser.baseUrl + 'template')
+  // realizar la prueba del ejercicio 3 para ese formulario
+  // 5. Probar la selección de un checkbox, para ello, seleccionar Cuba del checkbox Pais,
+  // sugerencia utilice la instruccion cssContainingText
+  // 6. Finalmente pruebe retrasos de tiempo jugando con el tiempo en la instrucción protractor.promise.delayed(tiempo);
+
+  // EJERCICIO SUGERIDO
+  // Consulte las pruebas del tutorial en la página de protractor https://www.protractortest.org/#/tutorial
+  // (si puede realice las pruebas del tutorial en un archivo de este proyecto ignorando las partes del archivo de configuración
+  // porque est proyecto ya cuenta con las configuraciones necesarias)
+
+  afterEach(async () => { // Este método se ejecuta despues de cada prueba
+    // Revisa si no hay errores severos emitidos por el navegador
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,53 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementArrayFinder, ElementFinder } from 'protractor';
 
-export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+export class ReactivePage { // clase de la página formularios reactive
+
+  title: ElementFinder;
+  nameinput: ElementFinder;
+  saveButton: ElementFinder;
+  addButton: ElementFinder;
+  deleteButton: ElementArrayFinder;
+  errorsText: ElementArrayFinder;
+
+  constructor() {
+    this.title = element(by.css('h4')); // obtenemos el elemento h4
+    this.nameinput = element(by.css('input[formControlName=nombre]')); // obtenemos en input por formControlName
+    this.saveButton = element(by.className('btn-outline-primary')); // obtenemos el elemento por el nombre de la clase
+    this.addButton = element(by.className('btn-success'));
+    this.errorsText = element.all(by.css('.text-danger')); // obtenemos todos los elementos con la clase text-danger
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  navigateToReactivePage(): Promise<unknown> { // navega a la ruta /reactive
+    return browser.get(browser.baseUrl + 'reactive') as Promise<unknown>;
+  }
+
+  getTitleText(): Promise<string> { // obtiene el titulo de la página
+    return this.title.getText() as Promise<string>;
+  }
+
+  setName(name: string): Promise<void>{ // escribe el el input name
+    this.nameinput.clear();
+    return this.nameinput.sendKeys(name) as Promise<void>;
+  }
+
+  getName(): Promise<string>{ // regresa el texto del input name
+    return this.nameinput.getAttribute('value') as Promise<string>;
+  }
+
+  clickSaveButton(): Promise<void>{ // presiona el botón guardar
+    return this.saveButton.click() as Promise<void>;
+  }
+
+  clickAddButton(): Promise<void>{ // presiona el botón añadir
+    return this.addButton.click() as Promise<void>;
+  }
+
+  deleteHobbieButtonIsPresent(): Promise<boolean>{ // comprueba si el botón borrar existe
+    this.addButton = element(by.className('btn-danger'));
+    return this.addButton.isPresent() as Promise<boolean>;
+  }
+
+  getTextOfEspecificError(indice: number): Promise<string>{ // obtiene el texto de un mensaje de error especifico
+    return this.errorsText.get(indice).getText() as Promise<string>;
   }
 }
